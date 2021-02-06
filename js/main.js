01af73117641fda4a5ead0a1c4ea6cd88a5089d2
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-02-06 11:32:23 +0300

    Добавил генератор фейковых данных

--- js/main.js
+++ js/main.js
@@ -41,3 +41,47 @@ function getRandomIntFloatInclusive(min = 0, max = 0, counter = 2) {
 
   return ((Math.random() * (max - min)) + min).toFixed(counter);
 }
+
+// Получение случайного элемента массива
+function getRandomElementFromArray(elements) {
+  return elements[getRandomIntInclusive(0, elements.length - 1)];
+}
+
+// Генератор фейковых данных
+function generateFakeData() {
+  const OFFER_TYPE = ['palace', 'flat', 'house', 'bungalow'];
+  const OFFER_TIME = ['12:00', '13:00', '14:00'];
+  const OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  const OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+  const LOCATION_X = getRandomIntFloatInclusive(35.65000, 35.70000, 5);
+  const LOCATION_Y = getRandomIntFloatInclusive(139.70000, 139.80000, 5);
+
+  const fakeData = {
+    author: {
+      avatar: 'img/avatars/user0' + getRandomIntInclusive(1, 8) + '.png',
+    },
+    location: {
+      x: LOCATION_X,
+      y: LOCATION_Y,
+    },
+    offer: {
+      title: 'title.' + new Date().getTime() * getRandomIntInclusive(1, 1000),
+      address: LOCATION_X + ', ' + LOCATION_Y,
+      price: getRandomIntInclusive(100, 1000),
+      type: getRandomElementFromArray(OFFER_TYPE),
+      rooms: getRandomIntInclusive(1, 10),
+      guests: getRandomIntInclusive(1, 5),
+      checkin: getRandomElementFromArray(OFFER_TIME),
+      checkout: getRandomElementFromArray(OFFER_TIME),
+      features: getRandomElementFromArray(OFFER_FEATURES),
+      description: 'description - ' + new Date().getTime() * getRandomIntInclusive(1, 1000) + ' - description',
+      photos: getRandomElementFromArray(OFFER_PHOTOS),
+    },
+  };
+
+  return fakeData;
+}
+
+const FAKE_COUNTER = 10;
+const fakes = new Array(FAKE_COUNTER).fill(null).map(() => generateFakeData());
+console.log(fakes);
