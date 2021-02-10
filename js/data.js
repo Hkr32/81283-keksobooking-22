2077ea56ef0bceb766d5e0c0b01ddc20b046c3de
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-02-10 16:30:29 +0300

    Разделение кода на части

---
+++ js/data.js
@@ -0,0 +1,8 @@
+const offer = {
+  types: ['palace', 'flat', 'house', 'bungalow'],
+  times: ['12:00', '13:00', '14:00'],
+  features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+}
+
+export { offer };
---
+++ js/generate.js
@@ -0,0 +1,42 @@
+import { offer } from './data.js';
+import {
+  getRandomIntInclusive,
+  getRandomIntFloatInclusive,
+  getRandomElementFromArray,
+  getRandomElementsFromArray
+} from './util.js';
+
+// Генератор фейковых данных
+function generateFakeData() {
+  const location = {
+    x: getRandomIntFloatInclusive(35.65000, 35.70000, 5),
+    y: getRandomIntFloatInclusive(139.70000, 139.80000, 5),
+  }
+
+  const fakeData = {
+    author: {
+      avatar: 'img/avatars/user0' + getRandomIntInclusive(1, 8) + '.png',
+    },
+    location: {
+      x: location.x,
+      y: location.y,
+    },
+    offer: {
+      title: 'title.' + new Date().getTime() * getRandomIntInclusive(1, 1000),
+      address: location.x + ', ' + location.y,
+      price: getRandomIntInclusive(100, 1000),
+      type: getRandomElementFromArray(offer.types),
+      rooms: getRandomIntInclusive(1, 10),
+      guests: getRandomIntInclusive(1, 5),
+      checkin: getRandomElementFromArray(offer.times),
+      checkout: getRandomElementFromArray(offer.times),
+      features: getRandomElementsFromArray(offer.features, true),
+      description: 'description - ' + new Date().getTime() * getRandomIntInclusive(1, 1000) + ' - description',
+      photos: getRandomElementsFromArray(offer.photos, true),
+    },
+  };
+
+  return fakeData;
+}
+
+export { generateFakeData };
