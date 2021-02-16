876da3baa031389e04f38c22f2d5ad5f6a2fe65e
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-02-16 11:08:32 +0300

    Вынес в отдельный модуль генерацию карточки

--- js/data.js
+++ js/data.js
@@ -2,7 +2,18 @@ const offer = {
   types: ['palace', 'flat', 'house', 'bungalow'],
   times: ['12:00', '13:00', '14:00'],
   features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-  photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
-}
+  photos: [
+    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+  ],
+};
 
-export { offer };
+const types = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalow: 'Бунгало',
+};
+
+export { offer, types };
