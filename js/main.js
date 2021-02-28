d012691b81d96a1ecc1cc845300e8979b4da79cc
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-02-28 10:50:27 +0300

    Вернул события для формы

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+import { filterFormHandler } from './form.js';
 import { disabledForm } from './helper.js';
 import { initMap } from './map.js';
+
+filterFormHandler(document.querySelector('.ad-form'));
 
 disabledForm(document.querySelector('.ad-form'), 'ad-form--disabled');
 disabledForm(document.querySelector('.map__filters'), 'map__filters--disabled');
