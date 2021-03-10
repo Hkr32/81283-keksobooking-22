203b1fecc21ee5fd29b9d731bb9d1aad91490f6d
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-03-10 11:41:32 +0300

    Добавил фильтр по остальным селектам

--- js/filter.js
+++ js/filter.js
@@ -18,9 +18,11 @@ function filterChangeHandler() {
   }
 }
 
-// 
+//
 function filterPoints() {
   const points = getPoints();
+
+  console.log(points)
 
   const housingForm = document.querySelector('form.map__filters');
   const housingType = housingForm.querySelector('#housing-type');
@@ -29,19 +31,59 @@ function filterPoints() {
   const housingGuests = housingForm.querySelector('#housing-guests');
   const housingFeatures = housingForm.querySelector('#housing-features');
 
-  
+
   let pointsFiltered = points;
-  
+
   if (housingType.value != 'any') {
     pointsFiltered = filterByType(pointsFiltered, housingType.value);
   }
+  if (housingPrice.value != 'any') {
+    pointsFiltered = filterByPrice(pointsFiltered, housingPrice.value);
+  }
+  if (housingRooms.value != 'any') {
+    pointsFiltered = filterByRooms(pointsFiltered, housingRooms.value);
+  }
+  if (housingGuests.value != 'any') {
+    pointsFiltered = filterByGuests(pointsFiltered, housingGuests.value);
+  }
 
-  return pointsFiltered.slice(0, pointsLimit);
+  return pointsFiltered.length > pointsLimit ? pointsFiltered.slice(0, pointsLimit) : pointsFiltered;
 }
 
 function filterByType(points, filterValue) {
   return points.filter((point) => {
     return point.offer.type == filterValue;
+  });
+}
+
+function filterByPrice(points, filterValue) {
+  switch (filterValue) {
+    case 'low':
+      return points.filter((point) => {
+        return point.offer.price < 10000;
+      });
+    case 'middle':
+      return points.filter((point) => {
+        return point.offer.price >= 10000 && point.offer.price <= 50000;
+      });
+    case 'high':
+      return points.filter((point) => {
+        return point.offer.price > 50000;
+      });
+    default:
+      return points;
+  }
+}
+
+function filterByRooms(points, filterValue) {
+  return points.filter((point) => {
+    return point.offer.rooms == filterValue;
+  });
+}
+
+function filterByGuests(points, filterValue) {
+  return points.filter((point) => {
+    return point.offer.guests == filterValue;
   });
 }
 
