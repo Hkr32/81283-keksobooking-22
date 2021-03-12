26aa66a80d2c9ecdab6d40e8ad5bcd4257fdfc9c
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-03-12 15:44:01 +0300

    Перенес статику в папку build

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import { adFormHandler } from './form.js';
-import { mapFormHandler } from './filter.js';
-import { initPage } from './page.js';
-
-adFormHandler(document.querySelector('.ad-form'));
-mapFormHandler(document.querySelector('.map__filters'));
-
-initPage();
