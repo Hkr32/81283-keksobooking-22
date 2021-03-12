89828e242de94ca0ff829b6accf9da9a251495c9
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-03-12 21:35:13 +0300

    Установил copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './source/js/main.js',
@@ -9,22 +10,47 @@ module.exports = {
     path: path.resolve(__dirname, 'build/'),
   },
   plugins: [
-    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "source/index.html",
+          to: "index.html",
+        },
+        {
+          from: "source/favicon.ico",
+          to: "favicon.ico",
+        },
+        {
+          from: "source/img/",
+          to: "img",
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].bundle.css'
+    }),
   ],
   module: {
     rules: [
       {
-        test: /\.(ttf|eot|svg|png|jpg|gif|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
+        test: /\.(ttf|eot|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+        options: {
+          name: '/fonts/[name].[ext]',
+        },
+      },
+      {
+        test: /\.(svg|png|jpg|gif|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+        options: {
+          name: '/img/[name].[ext]',
+        },
       },
       {
         test: /\.css$/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: '/build/css',
-            },
           },
           'css-loader',
         ],
